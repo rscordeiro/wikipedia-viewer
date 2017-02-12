246050c0055d18bd784f9935966b3a2e00d3f601
Roberto <roberto.sousacordeiro@gmail.com>
2017-02-12 16:45:34 -0200

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -1,10 +1,32 @@
 $(document).ready(function(){
   var myTerm;
-  var myAPI = 'https://en.wikipedia.org/w/api.php?';
-  const MAX_LOOP = 10; //if the limit value on data object changes, change this constante to match its value.
+  var myAPI = 'https://en.wikipedia.org/w/api.php';
+
+  $("#searchBar").autocomplete({
+    source: function(request, response) {
+      $.ajax({
+        url: myAPI,
+        dataType: 'jsonp',
+        data: {
+          'action': "opensearch",
+  	      'format': "json",
+  	      'search': request.term
+        },
+        success: function(data) {
+          response(data[1]);
+        }
+      });
+    },
+    minLength: 2,
+    select: function (event, ui) {
+      $("#searchBar").val(ui.item.value);
+    }
+  });
 
   var data;
   var mySearch = function() {
+    myTerm = $("#searchBar").val();
+    $("#result-div").empty();
     $.ajax({
       url: myAPI,
       dataType: 'jsonp',
@@ -19,12 +41,11 @@ $(document).ready(function(){
 	      "utf8": 1,
 	      "formatversion": "latest",
       },
-      success: function(response){
-        data = response;
+      success: function(responses){
+        data = responses;
         getResult(data);
-        console.log(data);
       },
-      error: function(response) {
+      error: function(responses) {
         alert("Error. Please try again.");
       }
     });
@@ -32,21 +53,19 @@ $(document).ready(function(){
 
   var getResult = function(result) {
     var resultsDivs;
-    for(var i = 0; i < MAX_LOOP; i++) {
-      resultsDivs += "<div class='col-xs-2 empty-div'></div><div class='col-xs-8 returned-div'><h3>" + result[1][i] + "</h3> <p>" + result[2][i] + "</p> <footer> <a target='_blank' href='" + result[3][i] + "'>" + result[3][i] + "</a> </footer> </div><br>";
+    for(var i = 0; i < result[1].length; i++) {
+      resultsDivs = "<div class='col-xs-2 empty-div'></div><div class='col-xs-8 returned-div'><h3><a target='_blank' href='" + result[3][i] + "'>" + result[1][i] + "</a> " + "</h3> <p>" + result[2][i] + "</p></div>";
+      $(resultsDivs).appendTo("#result-div");
     }
-    $("#result-div").html(resultsDivs);
   }
 
   $("#search-btn").click(function() {
-    myTerm = $("#searchBar").val();
     mySearch();
   });
 
-  /*$("#searchBar").keypress(function(e){
-    if (e.keyCode == 13) {
-      myTerm = $("#searchBar").val();
+  $("#searchBar").keypress(function(e){
+    if(e.keyCode == 13) {
       mySearch();
     }
-  });*/
+  });
 });
